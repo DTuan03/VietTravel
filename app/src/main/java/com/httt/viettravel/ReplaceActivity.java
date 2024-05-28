a9e4fba74a06ac25827d543ae1a4fe99c9670c1a
Đặng Anh Tuấn <137882845+DTuan03@users.noreply.github.com>
2024-05-28 17:05:47 +0700

    update

--- app/src/main/java/com/httt/viettravel/ReplaceActivity.java
+++ app/src/main/java/com/httt/viettravel/ReplaceActivity.java
@@ -3,6 +3,8 @@ package com.httt.viettravel;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +16,15 @@ import androidx.core.view.WindowInsetsCompat;
 public class ReplaceActivity extends AppCompatActivity {
 
     private ImageView imgBack;
+    private ImageView imgMatAnHienTai;
+    private ImageView imgMatAnMoi;
+    private ImageView imgMatAnNhapLai;
+    private EditText etMatKhauHienTai;
+    private EditText etMatKhauMoi;
+    private EditText etMatKhauNhapLai;
+
+    private Button btnCatNhat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,15 @@ public class ReplaceActivity extends AppCompatActivity {
 
     private void init(){
         imgBack = (ImageView) findViewById(R.id.activity_replace_img_back);
+        imgMatAnHienTai = (ImageView) findViewById(R.id.activity_replace_img_mat_an_hien_tai);
+        imgMatAnMoi = (ImageView) findViewById(R.id.activity_replace_img_mat_an_moi);
+        imgMatAnNhapLai = (ImageView) findViewById(R.id.activity_replace_img_mat_an_hien_tai);
+
+        etMatKhauHienTai = (EditText) findViewById(R.id.activity_replace_et_mat_khau_hien_tai);
+        etMatKhauMoi = (EditText) findViewById(R.id.activity_replace_et_mat_khau_moi);
+        etMatKhauNhapLai = (EditText) findViewById(R.id.activity_replace_et_mat_khau_nhap_lai);
+
+        btnCatNhat = (Button) findViewById(R.id.activity_replace_btn_cap_nhat);
     }
 
     private void setImgBack(){
