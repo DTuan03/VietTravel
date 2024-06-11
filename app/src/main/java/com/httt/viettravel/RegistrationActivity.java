c806dc7ebe216d1e7602c533f20e0c573f76dd46
Đặng Anh Tuấn <137882845+DTuan03@users.noreply.github.com>
2024-06-11 22:31:30 +0700

    Update

--- app/src/main/java/com/httt/viettravel/RegistrationActivity.java
+++ app/src/main/java/com/httt/viettravel/RegistrationActivity.java
@@ -65,15 +65,12 @@ public class RegistrationActivity extends AppCompatActivity {
         matAn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Log.d("TAG", "Tag hidden: " + R.mipmap.ic_launcher_eye_hidden_adaptive_fore);
                 if(v.getTag() == null || (int)v.getTag() == R.mipmap.ic_launcher_eye_hidden_adaptive_fore){
-                //    Log.d("RegistrationActivity", "Hien thi");
                     matAn.setImageResource(R.mipmap.ic_launcher_eye_adaptive_fore);
                     v.setTag(R.mipmap.ic_launcher_eye_adaptive_fore);
                     matKhau.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
 
                 }else{
-                //    Log.d("RegistrationActivity", "An");
                     matAn.setImageResource(R.mipmap.ic_launcher_eye_hidden_adaptive_fore);
                     v.setTag(R.mipmap.ic_launcher_eye_hidden_adaptive_fore);
                     matKhau.setTransformationMethod(PasswordTransformationMethod.getInstance());
@@ -93,13 +90,11 @@ public class RegistrationActivity extends AppCompatActivity {
     @SuppressLint("ResourceAsColor")
     private void ShowButton() {
         if (isEmpty(tenDayDu) || isEmpty(sdt) || isEmpty(matKhau) || isEmpty(nhapLai)) {
-           //Log.d("RegistrationActivity", "sau");
             regis.setEnabled(false);
             regis.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.regis_before)));
         }
         else{
             if(kiemTraMk()){
-                //Log.d("RegistrationActivity", "An");
                 regis.setEnabled(true);
                 regis.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.regis_after)));
             }else{
@@ -109,53 +104,19 @@ public class RegistrationActivity extends AppCompatActivity {
 
         }
     }
-
-    //su kien mat khau va nhap lai mat khau giong nhau
     private boolean kiemTraMk(){
         String matKhauText = matKhau.getText().toString();
         String nhapLaiText = nhapLai.getText().toString();
         if(!matKhauText.equals(nhapLaiText)){
-//            Log.d("Mat khau: " , "Mk: " + matKhau.getText().toString());
-//            Log.d("Nhap lai: " , "NL: " + nhapLai.getText().toString());
-//            Log.d("Trang thai: " , "tt: " +matKhau.equals(nhapLai));
             error.setText("Mật khẩu không khớp!");
             return false;
         }
         else{
-//            Log.d("Mat khau else: " , "Mk: " + matKhau.getText().toString());
-//            Log.d("Nhap lai else: " , "NL: " + nhapLai.getText().toString());
-//            Log.d("Trang thai else: " , "tt: " +matKhau.equals(nhapLai));
             error.setText(null);
             return true;
         }
     }
 
-    //theo doi su thay doi cua cac EditText
-//    private void setupTextWatchers() {
-//        TextWatcher textWatcher = new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                ShowButton();
-//                kiemTraMk();
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//
-//            }
-//        };
-//
-//        tenDayDu.addTextChangedListener(textWatcher);
-//        sdt.addTextChangedListener(textWatcher);
-//        matKhau.addTextChangedListener(textWatcher);
-//        nhapLai.addTextChangedListener(textWatcher);
-//
-//    }
 
     private void setButton(){
         tenDayDu.addTextChangedListener(new TextWatcher() {
--- app/src/main/java/com/httt/viettravel/ReplaceActivity.java
+++ app/src/main/java/com/httt/viettravel/ReplaceActivity.java
@@ -1,8 +1,12 @@
 package com.httt.viettravel;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +14,6 @@ import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,14 +21,14 @@ import androidx.core.view.WindowInsetsCompat;
 public class ReplaceActivity extends AppCompatActivity {
 
     private ImageView imgBack;
-    private ImageView imgMatAnHienTai;
-    private ImageView imgMatAnMoi;
-    private ImageView imgMatAnNhapLai;
-    private EditText etMatKhauHienTai;
-    private EditText etMatKhauMoi;
-    private EditText etMatKhauNhapLai;
+    private ImageView imgEyeHidenPresent;
+    private ImageView imgEyeHidenNew;
+    private ImageView imgEyeHidenCf;
+    private EditText etPassPresent;
+    private EditText etPassNew;
+    private EditText etPassCf;
 
-    private Button btnCatNhat;
+    private Button btnUpdate;
 
 
     @Override
@@ -40,19 +43,21 @@ public class ReplaceActivity extends AppCompatActivity {
         });
         init();
         setImgBack();
+        setButton();
     }
 
+    @SuppressLint("CutPasteId")
     private void init(){
         imgBack = (ImageView) findViewById(R.id.activity_replace_img_back);
-        imgMatAnHienTai = (ImageView) findViewById(R.id.activity_replace_img_mat_an_hien_tai);
-        imgMatAnMoi = (ImageView) findViewById(R.id.activity_replace_img_mat_an_moi);
-        imgMatAnNhapLai = (ImageView) findViewById(R.id.activity_replace_img_mat_an_hien_tai);
+        imgEyeHidenPresent = (ImageView) findViewById(R.id.activity_replace_img_eye_hiden_present);
+        imgEyeHidenNew = (ImageView) findViewById(R.id.activity_replace_img_eye_hide_new);
+        imgEyeHidenCf = (ImageView) findViewById(R.id.activity_replace_img_eye_hiden_present);
 
-        etMatKhauHienTai = (EditText) findViewById(R.id.activity_replace_et_mat_khau_hien_tai);
-        etMatKhauMoi = (EditText) findViewById(R.id.activity_replace_et_mat_khau_moi);
-        etMatKhauNhapLai = (EditText) findViewById(R.id.activity_replace_et_mat_khau_nhap_lai);
+        etPassPresent = (EditText) findViewById(R.id.activity_replace_et_pass_present);
+        etPassNew = (EditText) findViewById(R.id.activity_replace_et_pass_new);
+        etPassCf = (EditText) findViewById(R.id.activity_replace_et_pass_cf);
 
-        btnCatNhat = (Button) findViewById(R.id.activity_replace_btn_cap_nhat);
+        btnUpdate = (Button) findViewById(R.id.activity_replace_btn_update);
     }
 
     private void setImgBack(){
@@ -71,14 +76,70 @@ public class ReplaceActivity extends AppCompatActivity {
         return (string.isEmpty());
     }
 
-    private void setButton(){
-        if(isEmpty(etMatKhauHienTai) || isEmpty(etMatKhauMoi) || isEmpty(etMatKhauNhapLai)){
-            btnCatNhat.setEnabled(false);
-            btnCatNhat.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.regis_before)));
+    private void showButton(){
+        if(isEmpty(etPassPresent) || isEmpty(etPassNew) || isEmpty(etPassCf)){
+            btnUpdate.setEnabled(false);
+            Log.d("CheckPass", "Null");
+            btnUpdate.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.regis_before)));
         }else{
-            btnCatNhat.setEnabled(true);
-            btnCatNhat.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.regis_after)));
+            btnUpdate.setEnabled(true);
+            Log.d("CheckPass", "NotNull");
+            btnUpdate.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.replace_activity_btn)));
         }
+
+    }
+
+    private void setButton(){
+        etPassPresent.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                showButton();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        etPassNew.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                showButton();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        etPassCf.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                showButton();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
 
