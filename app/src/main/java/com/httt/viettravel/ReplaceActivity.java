bccc08c8f8263cce1a34688f0f3ac8fab2f7a3a0
Đặng Anh Tuấn <137882845+DTuan03@users.noreply.github.com>
2024-06-11 23:15:24 +0700

    update ReplacePass

--- app/src/main/java/com/httt/viettravel/ReplaceActivity.java
+++ app/src/main/java/com/httt/viettravel/ReplaceActivity.java
@@ -6,11 +6,14 @@ import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,13 +24,9 @@ import androidx.core.view.WindowInsetsCompat;
 public class ReplaceActivity extends AppCompatActivity {
 
     private ImageView imgBack;
-    private ImageView imgEyeHidenPresent;
-    private ImageView imgEyeHidenNew;
-    private ImageView imgEyeHidenCf;
-    private EditText etPassPresent;
-    private EditText etPassNew;
-    private EditText etPassCf;
-
+    private ImageView imgEyeHiddenPresent, imgEyeHiddenNew, imgEyeHiddenCf;
+    private EditText etPassPresent, etPassNew, etPassCf;
+    private TextView etNotify;
     private Button btnUpdate;
 
 
@@ -43,19 +42,23 @@ public class ReplaceActivity extends AppCompatActivity {
         });
         init();
         setImgBack();
+        clickImageView(imgEyeHiddenPresent, etPassPresent);
+        clickImageView(imgEyeHiddenNew,etPassNew);
+        clickImageView(imgEyeHiddenCf, etPassCf);
         setButton();
     }
 
     @SuppressLint("CutPasteId")
     private void init(){
         imgBack = (ImageView) findViewById(R.id.activity_replace_img_back);
-        imgEyeHidenPresent = (ImageView) findViewById(R.id.activity_replace_img_eye_hiden_present);
-        imgEyeHidenNew = (ImageView) findViewById(R.id.activity_replace_img_eye_hide_new);
-        imgEyeHidenCf = (ImageView) findViewById(R.id.activity_replace_img_eye_hiden_present);
+        imgEyeHiddenPresent = (ImageView) findViewById(R.id.activity_replace_img_eye_hiden_present);
+        imgEyeHiddenNew = (ImageView) findViewById(R.id.activity_replace_img_eye_hide_new);
+        imgEyeHiddenCf = (ImageView) findViewById(R.id.activity_replace_img_eye_hiden_cf);
 
         etPassPresent = (EditText) findViewById(R.id.activity_replace_et_pass_present);
         etPassNew = (EditText) findViewById(R.id.activity_replace_et_pass_new);
         etPassCf = (EditText) findViewById(R.id.activity_replace_et_pass_cf);
+        etNotify = (TextView) findViewById(R.id.activity_replace_tv_notifi);
 
         btnUpdate = (Button) findViewById(R.id.activity_replace_btn_update);
     }
@@ -87,6 +90,13 @@ public class ReplaceActivity extends AppCompatActivity {
             btnUpdate.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.replace_activity_btn)));
         }
 
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void checkPassCf(){
+        if(!etPassCf.getText().toString().equals(etPassNew.getText().toString())){
+            etNotify.setText("Mật khẩu nhập lại không khớp");
+        }
     }
 
     private void setButton(){
@@ -133,6 +143,7 @@ public class ReplaceActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 showButton();
+                checkPassCf();
             }
 
             @Override
@@ -142,5 +153,28 @@ public class ReplaceActivity extends AppCompatActivity {
         });
     }
 
+    private void clickImageView(ImageView eyeHidden, EditText pass){
+        eyeHidden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getTag() == null || (int)v.getTag() == R.mipmap.ic_launcher_eye_hidden_adaptive_fore){
+                    eyeHidden.setImageResource(R.mipmap.ic_launcher_eye_adaptive_fore);
+                    v.setTag(R.mipmap.ic_launcher_eye_adaptive_fore);
+                    Log.d("Pass", "Pass");
+                    pass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    Log.d("Pass", "Pass2");
 
+                }else{
+                    eyeHidden.setImageResource(R.mipmap.ic_launcher_eye_hidden_adaptive_fore);
+                    v.setTag(R.mipmap.ic_launcher_eye_hidden_adaptive_fore);
+                    pass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+//                Dùng để kiểu như khi an vao mat thi no se kh con o trong edittext nua
+//                neu bo di thi khi an vao mat no se chuyen ve dau edittext de nhạp, thuong neu muon thi phải bat sk cho no ve cuoi edtect
+//                o day cho no ra khoi edtext luon
+                pass.requestFocus();
+                pass.clearFocus();
+            }
+        });
+    }
 }
