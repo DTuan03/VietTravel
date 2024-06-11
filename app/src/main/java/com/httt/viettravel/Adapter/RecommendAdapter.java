e0975241fce9382ce2c2ab80523eca5763bf9dbe
Đặng Anh Tuấn <137882845+DTuan03@users.noreply.github.com>
2024-06-11 18:35:28 +0700

    update

--- app/src/main/java/com/httt/viettravel/Adapter/RecommendAdapter.java
+++ app/src/main/java/com/httt/viettravel/Adapter/RecommendAdapter.java
@@ -1,24 +1,62 @@
 package com.httt.viettravel.Adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.httt.viettravel.Model.Recommend;
 import com.httt.viettravel.R;
 
-public class RecommendAdapter {
+import java.util.List;
 
-    public class RecommentAdapter extends RecyclerView.ViewHolder{
+public class RecommendAdapter extends RecyclerView.Adapter <RecommendAdapter.RecommentViewHolder> {
+    private List<Recommend> recommendList;
+
+    public void setData(List<Recommend> list){
+        this.recommendList = list;
+    }
+    @NonNull
+    @Override
+    public RecommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recommend, parent, false);
+        return new RecommentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecommentViewHolder holder, int position) {
+        Recommend recommend = recommendList.get(position);
+        if (recommend != null){
+            holder.img.setImageResource(recommend.getResourceId());
+            holder.title.setText(recommend.getTitle());
+            holder.numberStar.setText(recommend.getNumberStar());
+            holder.price.setText(recommend.getPrice());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (recommendList != null){
+            return recommendList.size();
+        }
+        return 0;
+    }
+
+    public class RecommentViewHolder extends RecyclerView.ViewHolder{
         private ImageView img;
         private TextView title;
         private TextView numberStar;
         private TextView price;
-        public RecommentAdapter(@NonNull View itemView) {
+        public RecommentViewHolder (@NonNull View itemView) {
             super(itemView);
-            img = itemView.findViewById(R.id.item)
+            img = itemView.findViewById(R.id.item_recommend_img);
+            title = itemView.findViewById(R.id.item_recommend_tv_title);
+            numberStar = itemView.findViewById(R.id.item_recommend_tv_number_star);
+            price = itemView.findViewById(R.id.item_recommend_price);
         }
     }
 }
--- app/src/main/java/com/httt/viettravel/RecommendFragment.java
+++ app/src/main/java/com/httt/viettravel/RecommendFragment.java
@@ -3,12 +3,23 @@ package com.httt.viettravel;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.httt.viettravel.Adapter.RecommendAdapter;
+import com.httt.viettravel.Model.Recommend;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecommendFragment extends Fragment {
+
+    private RecommendAdapter recommendAdapter;
+    private RecyclerView recyclerView;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -40,7 +51,43 @@ public class RecommendFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recommend, container, false);
+        init(view);
+        recommendAdapter = new RecommendAdapter();
+
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        
+        recommendAdapter.setData(getList());
+
+        recyclerView.setAdapter(recommendAdapter);
 
         return view;
+    }
+
+    private void init(View view){
+        recyclerView = view.findViewById(R.id.fragment_recommend_rcv);
+    }
+
+    private List<Recommend> getList(){
+        List<Recommend> list = new ArrayList<>();
+        list.add(new Recommend(R.drawable.hue5,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.hue1,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.hue1,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+        list.add(new Recommend(R.drawable.qpc,"Tour tham quan lang tam huong quang phu cau lang nghe truyen thong o Ha Noi","5.0", "840,000"));
+
+        return list;
     }
 }
